1c5db3bbbea632823c7302466f807ba4dd854589
song <1651037998@qq.com>
2014-10-13 21:24:23 +0800

    ff

--- app/js/aboutus.js
+++ app/js/aboutus.js
@@ -1,10 +1,12 @@
-$(function(){
-	$(".aboutMore").on('click',function(){
-		var oldLeft = parseInt($(".aboutWra").css('left').replace('px',''));
-		$(".aboutWra").animate({ left: oldLeft - 908 + 'px' }, "slow");
-	})
-	$(".aboutBack").on('click',function(){
-		var oldLeft = parseInt($(".aboutWra").css('left').replace('px',''));
-		$(".aboutWra").animate({ left: oldLeft + 908 + 'px' }, "slow");
-	})
+define(function (require, exports, module) {
+	(function(){
+		$(".aboutMore").on('click',function(){
+			var oldLeft = parseInt($(".aboutWra").css('left').replace('px',''));
+			$(".aboutWra").animate({ left: oldLeft - 908 + 'px' }, "slow");
+		})
+		$(".aboutBack").on('click',function(){
+			var oldLeft = parseInt($(".aboutWra").css('left').replace('px',''));
+			$(".aboutWra").animate({ left: oldLeft + 908 + 'px' }, "slow");
+		})
+	})()
 })
