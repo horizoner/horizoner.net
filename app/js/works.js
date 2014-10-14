d33f2605c4d540a6ad67b919ed2b42ccadd99a80
Keith <lufengd3@gmail.com>
2014-10-14 20:45:21 +0800

    update

--- app/js/works.js
+++ app/js/works.js
@@ -3,8 +3,34 @@ define(function (require, exports, module) {
 		$(".worksItem").click(function() {
 			$('.worksInfoLayer').css('display', 'block');
 		});
+
+		$('.worksMore').click(function() {
+			$.post('getMoreWorks.php', {'lastId': $('#worksLastId').val()}, function(data, error) {
+				if (error != null) {
+					alert('Error: ' + error);
+				} else {
+					// change nav
+					$('.worksMore').css('display', 'none');
+					$('.worksMoreLeft').css('display', 'inline-block');
+					$('.worksMoreRight').css('display', 'inline-block');
+					var i = 0;
+					// replace all works
+					$.each($('.worksItem'), function() {
+						if (i < data.content.length) {
+							$(this).find('img').attr('src', data.content[i].img);
+							$(this).find('h3').attr('name', data.content[i].name);
+							$(this).find('p').attr('info', data.content[i].info);
+							$('#worksLastId').val(data.content[i].id)
+						}
+					});
+				}
+			}, 'json');
+		});
+		
 		$('.worksCloseLayer').click(function() {
 			$('.worksInfoLayer').css('display', 'none');
-		})
+		});
+
+
  	})();
 })
