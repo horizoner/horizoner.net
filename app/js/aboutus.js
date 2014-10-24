577d3d515d419793e730eb9dba6f414a5bc5e178
song <1651037998@qq.com>
2014-10-24 20:41:09 +0800

    ff

--- app/js/aboutus.js
+++ app/js/aboutus.js
@@ -1,12 +1,34 @@
 define(function (require, exports, module) {
 	(function(){
-		$(".aboutMore").on('click',function(){
-			var oldLeft = parseInt($(".aboutWra").css('left').replace('px',''));
-			$(".aboutWra").animate({ left: oldLeft - 936 + 'px' }, "slow");
-		})
-		$(".aboutBack").on('click',function(){
-			var oldLeft = parseInt($(".aboutWra").css('left').replace('px',''));
-			$(".aboutWra").animate({ left: oldLeft + 936 + 'px' }, "slow");
-		})
+	    $(".aboutBack").hide();
+		$('.aboutMore').click(function() {
+		    $.ajax({
+		        type:"post",
+		        data:{"changeId": $("#aboutLastId").val()},
+		        url:"getAboutMore.php",
+		        dataType:"json",
+		        success:function(data){
+		            $(".aboutMore").show();
+		            $(".aboutBack").show();
+		            var i = 0;
+		            $.each($(".aboutHead"),function(){
+		            	if (i < data.content.length) {
+		                    $(this).find("img").attr("src", data.content[i].img);
+							$(this).find(".aboutName").text(data.content[i].aboutName);
+							$(this).find(".aboutEdu").text(data.content[i].aboutEdu);
+							$(this).find(".aboutDire").text(data.content[i].aboutDire);
+							$(this).find(".aboutCompany").text(data.content[i].aboutCompany);
+							$("#aboutLastId").val(data.content[i].id)
+		                };
+		            });
+		        },
+		        error: function(data) {
+                        if (data != null) {
+					    alert("请求错误！");
+                        };
+                }
+
+		    });
+		});
 	})()
 })
